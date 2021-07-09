b955576bee051ff71e0d82392307911f17873c04
SakaAblage <31892698+SakaAblage@users.noreply.github.com>
2021-07-09 20:23:33 +0200

    Update physikFormeln.h

--- physikFormeln.h
+++ physikFormeln.h
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
 //Variablendeklaration
-int Geschwindigkeit;//v
-int Beschleunigung; //a 
-int Impuls; 		//p 
-int Weg; 			//s 
-int Zeit;			//t 
-int Masse; 			//m 
+
+int Geschwindigkeit;		  //v
+int startGeschwindigkeit 	//v1
+int endGeschwindigkeit		//v2
+
+int Beschleunigung; 		  //a 
+
+int Impuls; 				      //p 
+
+int Weg; 				        	//s 
+int startWeg; 	      		//s1
+int endWeg;			      		//s2
+
+int Zeit;				        	//t 
+int startZeit;	    			//t1
+int endZeit;		      		//t2
+
+int Masse; 			      		//m 
